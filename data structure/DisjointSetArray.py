ec2629fa6e83bfe8f6b7f051d14e947443922271
as468579 <as468579@gmail.com>
2021-12-01 00:58:45 +0800

    modify TestCase of DisjointSetArray

--- data structure/DisjointSetArray.py
+++ data structure/DisjointSetArray.py
@@ -1,7 +1,16 @@
 
+from typing import Sized
+
+
 class DisjointSetArray:
     def __init__(self, arr):
         self.groups = list(arr)
+        self.size   = [0 for _ in self.groups]
+
+        # Count number of elements in each groups
+        for g in self.groups:
+            self.size[g] += 1
+        
 
     def isEquivalent(self, ds2):
         for (g1, g2) in zip(self.groups, ds2.groups):
@@ -10,10 +19,32 @@ class DisjointSetArray:
         return True
 
     def find(self, x):
+        return self.groups[x]
+
+    # Merge the sets that belong to x and y individually
+    def merge(self, x, y):
+        if self.groups[x] == self.groups[y]: return 
+
+        gsmall, glarge = self.groups[x], self.groups[y]
+        if self.size[self.groups[x]] > self.size[self.groups[y]]:
+            gsmall, glarge = self.groups[y], self.groups[x]
         
+        self.size[glarge] += self.size[gsmall]
+        self.size[gsmall] = 0
+        for index in range(len(self.groups)):
+
+            if self.groups[index] == gsmall:
+                self.groups[index] = glarge
+
+
+
+    # Calculate the cardinality of the set which x belongs to
+    def getCardinality(self, x):
+        return self.size[self.groups[x]]
+
     # def merge(self, x, y):
     
-    # def getCardinality(self, x):
+
 
     # def isSingleton(self, x):
 
--- data structure/DisjointSetTestCase.py
+++ data structure/DisjointSetTestCase.py
@@ -4,12 +4,41 @@ from DisjointSetArray import DisjointSetArray
 class DisjointSetTestCase(unittest.TestCase):
 
     # def setUp(self):
+
     # def tearDown(self):
-    def test_isEquivalent(self):
+    def testIsEquivalent(self):
         ds1 = DisjointSetArray(range(5))
         ds2 = DisjointSetArray(range(5))
         res = ds1.isEquivalent(ds2)
         self.assertTrue(res)
+
+    def testFind(self):
+        ds1 = DisjointSetArray(range(5))
+        for i in range(5):
+            self.assertEqual(ds1.find(i), i)
+
+    def testMerge(self):
+        ds1 = DisjointSetArray([0, 0, 2 ,2, 4, 5, 2, 4, 2, 2])
+        ds2 = DisjointSetArray([0, 0, 2 ,2, 4, 5, 2, 4, 2, 2])
+
+        ds1.merge(2, 3)
+        self.assertTrue(ds1.isEquivalent(ds2))
+        expected_cardinality = [2, 2, 5, 5, 2, 1, 5, 2, 5, 5]
+        for i in range(len(expected_cardinality)):
+            self.assertEqual(expected_cardinality[i], ds1.getCardinality(i))
+
+        ds3 = DisjointSetArray([0, 0, 2, 2, 2, 5, 2, 2, 2, 2])
+        ds1.merge(3, 7)
+        self.assertTrue(ds1.isEquivalent(ds3))
+        
+
+    def testGetCardinality(self):
+        arr      = [0, 0, 2 ,2, 4, 5, 2, 4, 2, 2]
+        expected = [2, 2, 5, 5, 2, 1, 5, 2, 5, 5]
+        ds = DisjointSetArray(arr)
+        for i in range(len(arr)):
+            self.assertEqual(ds.getCardinality(i), expected[i])
+
 
 if __name__ == '__main__':
     unittest.main()
