85204bdd9f13cd9aec0c60e7ee4598b209fd4e46
as468579 <as468579@gmail.com>
2022-01-12 22:21:43 +0800

    add 120.Triangle [SC:O(1)]

--- cplusplus/medium/Triangle.cpp
+++ cplusplus/medium/Triangle.cpp
@@ -2,6 +2,33 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
+        for(int i=1; i<triangle.size(); ++i){
+            for(int j=0; j<=i; ++j){
+                if(j && (j!=i)){
+                    
+                    triangle[i][j] = min(triangle[i-1][j-1], triangle[i-1][j]) + triangle[i][j];
+                }
+                else if(j){
+                    triangle[i][j] = triangle[i-1][j-1] + triangle[i][j];
+                }
+                else{
+                    triangle[i][j] = triangle[i-1][j] + triangle[i][j];
+                }
+            }
+        }
+        
+        
+        int min_value = INT_MAX;
+        for(int j=0; j<triangle.size(); ++j){
+            min_value = min(min_value, triangle[triangle.size()-1][j]);
+        }
+        return min_value;
+    }
+};
+/* Space Complexity: O(2n)
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
         
         vector<int> prev(triangle.size(), 100000);
         vector<int> curr(triangle.size(), 100000);
@@ -30,6 +57,7 @@ public:
         return min;
     }
 };
+*/
 
 
 /* Space Complexity: O(n^2)
