b885cc4f73eddefcf9dea5a81bf60c40518d1d83
as468579 <as468579@gmail.com>
2022-03-09 19:05:20 +0800

    add 82.Remove Duplicates from Sorted List II [iterative version]

--- cplusplus/medium/RemoveDuplicatesFromSortedListII.cpp
+++ cplusplus/medium/RemoveDuplicatesFromSortedListII.cpp
@@ -8,6 +8,10 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+// Recursive Version
+/*
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -31,4 +35,43 @@ public:
             return head;
         }
     }   
+};
+*/
+
+// Iterative Version
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        if(!head || !(head->next)){ return head; }
+        ListNode *prev=nullptr, *curr=head, *rm_tmp;
+        bool remove=false;
+        
+        while(curr){
+            
+            while((curr->next) && (curr->val == curr->next->val)){
+                remove = true;
+                rm_tmp = curr->next;
+                curr->next = curr->next->next;
+                delete rm_tmp;
+            }    
+            
+            if(remove){
+                remove = false;
+                rm_tmp = curr;
+                curr = curr->next;
+                delete rm_tmp;
+                
+                if(prev){
+                    prev->next = curr;
+                }else{
+                    head = curr;
+                }
+            }else{
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return head;
+    }    
 };
